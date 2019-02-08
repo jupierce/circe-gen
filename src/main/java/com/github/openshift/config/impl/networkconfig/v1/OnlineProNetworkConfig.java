0fc751ebd2f860d0af3e3c02f313735d3708a535
jupierce <jupierce@redhat.com>
2019-02-08 13:12:23 -0500

    Removing cluster specific implementation from library

--- src/main/java/com/github/openshift/config/impl/networkconfig/v1/OnlineProNetworkConfig.java
+++
@@ -1,5 +0,0 @@
-package com.github.openshift.config.impl.networkconfig.v1;
-
-
-public class OnlineProNetworkConfig extends OnlineNetworkConfig {
-}
--- src/main/java/com/github/openshift/config/impl/networkconfig/v1/OnlineStarterNetworkConfig.java
+++
@@ -1,5 +0,0 @@
-package com.github.openshift.config.impl.networkconfig.v1;
-
-
-public class OnlineStarterNetworkConfig extends OnlineNetworkConfig {
-}
