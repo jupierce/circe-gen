2a746fb7e9f62619ad6a36d70c525435ff373edd
jupierce <jupierce@redhat.com>
2019-03-01 17:16:33 -0500

    Removing redundant definition

--- src/generated/java/com/github/openshift/circe/gen/DefinitionType.java
+++ src/generated/java/com/github/openshift/circe/gen/DefinitionType.java
@@ -5,11 +5,10 @@ public enum DefinitionType {
 
 	v4_0_network(com.github.openshift.circe.gen.def.v4_0.NetworkDefinition.class, "network", "v4.0-network"),
 	v4_0_project(com.github.openshift.circe.gen.def.v4_0.ProjectConfigDefinition.class, "project", "v4.0-project"),
-	v4_0_build(com.github.openshift.circe.gen.def.v4_0.BuildConfigDefinition.class, "build", "v4.0-build"),
+	v4_0_build(com.github.openshift.circe.gen.def.v4_0.BuildDefinition.class, "build", "v4.0-build"),
 	v4_0_auth(com.github.openshift.circe.gen.def.v4_0.AuthDefinition.class, "auth", "v4.0-auth"),
 	v4_0_machineconfig(com.github.openshift.circe.gen.def.v4_0.MachineConfigDefinition.class, "machineconfig", "v4.0-machineconfig"),
 	v4_0_controlplane(com.github.openshift.circe.gen.def.v4_0.ControlPlaneConfigDefinition.class, "controlplane", "v4.0-controlplane"),
-	v4_0_builds(com.github.openshift.circe.gen.def.v4_0.BuildConfigDefinition.class, "builds", "v4.0-builds"),
 	v4_0_console(com.github.openshift.circe.gen.def.v4_0.ConsoleDefinition.class, "console", "v4.0-console"),
 	;
 
--- src/generated/java/com/github/openshift/circe/gen/def/v4_0/BuildDefinition.java
+++ src/generated/java/com/github/openshift/circe/gen/def/v4_0/BuildDefinition.java
@@ -5,12 +5,12 @@ import com.github.openshift.circe.yaml.*;
 import com.github.openshift.circe.gen.build.v1.*;
 import com.github.openshift.circe.beans.*;
 
-public interface BuildConfigDefinition extends Definition {
+public interface BuildDefinition extends Definition {
 
 	@RenderOrder(value ="0001")
 	Build getBuild() throws Exception;
 
-	interface EZ extends BuildConfigDefinition {
+	interface EZ extends BuildDefinition {
 
 		@RenderOrder(value ="0001")
 		default Build getBuild() throws Exception { return null; }
